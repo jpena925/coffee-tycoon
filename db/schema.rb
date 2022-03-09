8a9816107804f21b0d058a686621f686a29c6a87
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-03-09 14:04:15 -0800

    just making sure it's all there

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2022_03_07_221959) do
     t.float "sell_price"
     t.integer "menu_item_id"
     t.integer "customer_id"
-    t.boolean "fulfilled"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["menu_item_id"], name: "index_orders_on_menu_item_id"
   end
